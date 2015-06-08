d45eb4f5934075e10f7031d0036f986d061c0c2c
Bryan Spahr <bspahr314@gmail.com>
2015-06-08 16:37:27 -0500

    live query of databases in dropdown

--- src/main/webapp/components/reports/chartController.js
+++ src/main/webapp/components/reports/chartController.js
@@ -1,6 +1,7 @@
 app.controller('chartControl', function ($scope, dataSourceService){
 	$scope.pageTitle = "build a chart";
 	$scope.databases = {};
+	$scope.chosenDatabase = "";
 	
 	var databasePromise = dataSourceService.getDatabases();
 	databasePromise.then(function(result){
