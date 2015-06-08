6f7373636f162739fc944811aa56b7d1de34c710
Bryan Spahr <bspahr314@gmail.com>
2015-06-08 16:54:57 -0500

    removed junk url

--- src/main/webapp/services/dataSourceService.js
+++ src/main/webapp/services/dataSourceService.js
@@ -3,7 +3,6 @@ app.factory('dataSourceService', function($http){
 	
 	factory.getDatabases = function(){
 		var url = "http://172.16.16.64/perfmon4j/rest/datasource/databases";
-//		var url = "http://www.w3schools.com/angular/customers_sql.aspx";
 		
 		return $http.get(url).then(function(result){
 			return result;
