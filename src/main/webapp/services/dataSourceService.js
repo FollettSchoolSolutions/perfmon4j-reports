3c29617b552c07a7eac70bc960f0dd50c3a7384e
Bryan Spahr <bspahr314@gmail.com>
2015-06-08 11:51:37 -0500

    correct database path

--- src/main/webapp/services/dataSourceService.js
+++ src/main/webapp/services/dataSourceService.js
@@ -2,8 +2,8 @@ app.factory('dataSourceService', function($http){
 	var factory = {};
 	
 	factory.getDatabases = function(){
-//		var url = "http://172.16.16.85:8080/RestDataSource/rest/datasource/databases";
-		var url = "http://www.w3schools.com/angular/customers_sql.aspx";
+		var url = "http://172.16.16.64/perfmon4j/rest/datasource/databases";
+//		var url = "http://www.w3schools.com/angular/customers_sql.aspx";
 		
 		return $http.get(url).then(function(result){
 			return result;
