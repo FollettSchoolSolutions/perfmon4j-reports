6def0cef5d8be9d908a6d1435021ea35f22778fc
Bryan Spahr <bspahr314@gmail.com>
2015-06-10 13:57:25 -0500

    new official logo and grey color scheme applied

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,1 +1,8 @@
 var app = angular.module("perfmonReportsApp", ['ngMaterial','ngMdIcons']);
+app.config(function($mdThemingProvider) {
+	$mdThemingProvider.theme('default')
+	  .primaryPalette('grey', {
+		  'default': '700'
+	  })
+	  .accentPalette('blue-grey');
+});
