cae08c7aa364c67b4e9faaba532d2f2b6a827e6e
Caitlin Deuchert <caitlin@deuchert.com>
2015-06-11 16:45:00 -0500

    Got a chart up and running, querying from live data!!! Also included the files needed to run c3 and d3

--- src/main/webapp/services/dataSourceService.js
+++ src/main/webapp/services/dataSourceService.js
@@ -9,5 +9,14 @@ app.factory('dataSourceService', function($http){
 		})
 	}
 	
+	factory.getStuff = function(){
+		var url = "http://172.16.16.64/perfmon4j/rest/datasource/databases/default/observations.c3?seriesDefinition=MFSS-DEVL.24~MFSS-DEVL.25~Interval.WebRequest~maxActiveThreads_MFSS-DEVL.24~MFSS-DEVL.25~Interval.WebRequest~averageDuration_MFSS-DEVL.24~MFSS-DEVL.25~JVM~systemCpuLoad&seriesAlias=Threads_Average_CPU";
+		
+		return $http.get(url).then(function(result){
+			return result;
+		})
+	}
+	
+	
 	return factory;
 });
