3383b7ee9d8878750dd12e10b932ef0a415d6c73
Caitlin Deuchert <caitlin@deuchert.com>
2015-07-20 15:00:09 -0500

    Persisting charts now works for multiple systems. Multiple systems are saved as comma separated values.

--- src/main/java/org/perfmon4j/reports/app/entity/Series.java
+++ src/main/java/org/perfmon4j/reports/app/entity/Series.java
@@ -31,7 +31,7 @@ public class Series {
 	private String name;
 	
 	@Column(nullable = false)
-	private String system;
+	private String systems;
 	
 	@Column(nullable = false)
 	private String category;
@@ -47,7 +47,7 @@ public class Series {
 	
 	public Series(SeriesVo svo){
 		this.name = svo.getName();
-		this.system = svo.getSystem();
+		this.systems = svo.getSystems();
 		this.category = svo.getCategory();
 		this.field = svo.getField();
 		this.aggregationMethod = svo.getAggregationMethod();
@@ -73,12 +73,12 @@ public class Series {
 		this.name = name;
 	}
 
-	public String getSystem() {
-		return system;
+	public String getSystems() {
+		return systems;
 	}
 
-	public void setSystem(String system) {
-		this.system = system;
+	public void setSystems(String systems) {
+		this.systems = systems;
 	}
 
 	public String getCategory() {
